bf0a4143c7f4d816b26fac72a88e4117fa2d5d8f
Benjamin Müller <benimueller@outlook.com>
2018-04-20 12:57:59 +0200

    Add unit of work interface

---
+++ Backend/src/SSAH.Core/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace SSAH.Core
+{
+    public class Constants
+    {
+        public struct UnitOfWork
+        {
+            public const string TOP_LEVEL_LIFETIME_SCOPE_TAG = "TOP_LEVEL_UNIT_OF_WORK";
+        }
+    }
+}
