1fc520cc3e075c0fbb558a4726dbceb501c0c331
Benjamin Müller <benimueller@outlook.com>
2018-05-09 09:47:46 +0200

    Add reactive extensions

---
+++ Backend/src/SSAH.Core/Messaging/Queue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+
+namespace SSAH.Core.Messaging
+{
+    public class Queue : IObservable<IEvent>
+    {
+        public Queue()
+        {
+            var x = new EventLoopScheduler();
+            var obs = new Subject<IEvent>();
+            obs.
+        }
+
+    }
+}
