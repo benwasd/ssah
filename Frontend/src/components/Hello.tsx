0b0fa54a4486645872eacfe87f9b490214ad76c3
Benjamin Müller <benimueller@outlook.com>
2018-05-11 17:54:53 +0200

    Test inline condition

--- Frontend/src/components/Hello.tsx
+++ Frontend/src/components/Hello.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Flag, Button, Grid, Header, List, Segment, Accordion, Icon } from 'semantic-ui-react';
+import { Flag, Button, Grid, Header, List, Segment, Accordion, Icon, Modal, } from 'semantic-ui-react';
 
 export interface HelloProps { compiler: string; framework: string; }
 
@@ -18,45 +18,73 @@ export class Hello extends React.Component<HelloProps, {}> {
         const { activeIndex } = this.state
 
         return (
-            <Accordion fluid styled>
-                <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
-                    <Icon name='dropdown' />
-                    <Flag name='switzerland' />
-                    What is a dog?
-                </Accordion.Title>
-                <Accordion.Content active={activeIndex === 0}>
-                    <p>
-                        A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a
-                        {' '}welcome guest in many households across the world.
-                    </p>
-                </Accordion.Content>
+            <div>
+                {false && (
+                        <Header as='h2' icon>
+                            <Icon name='settings' />
+                            Account Settings
+                            <Header.Subheader>
+                                Manage your account settings and set e-mail preferences.
+                            </Header.Subheader>
+                        </Header>
+                    )
+                }
 
-                <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
-                    <Icon name='dropdown' />
-                    What kinds of dogs are there?
-                </Accordion.Title>
-                <Accordion.Content active={activeIndex === 1}>
-                    <p>
-                        There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of
-                        {' '}dog that they find to be compatible with their own lifestyle and desires from a companion.
-                    </p>
-                </Accordion.Content>
 
-                <Accordion.Title active={activeIndex === 2} index={2} onClick={this.handleClick}>
-                    <Icon name='dropdown' />
-                    How do you acquire a dog?
-                </Accordion.Title>
-                <Accordion.Content active={activeIndex === 2}>
-                    <p>
-                        Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.
-                    </p>
-                    <p>
-                        A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to
-                        {' '}assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your
-                        {' '}dog from a shelter, helps give a good home to a dog who may not find one so readily.
-                    </p>
-                </Accordion.Content>
-            </Accordion>
+                <Accordion fluid styled>
+                    <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+                        <Icon name='dropdown' />
+                        <Flag name='switzerland' />
+                        What is a dog?
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex === 0}>
+                        <p>
+                            A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a
+                            {' '}welcome guest in many households across the world.
+                        </p>
+                    </Accordion.Content>
+
+                    <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
+                        <Icon name='dropdown' />
+                        What kinds of dogs are there?
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex === 1}>
+                        <p>
+                            There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of
+                            {' '}dog that they find to be compatible with their own lifestyle and desires from a companion.
+                        </p>
+                    </Accordion.Content>
+
+                    <Accordion.Title active={activeIndex === 2} index={2} onClick={this.handleClick}>
+                        <Icon name='dropdown' />
+                        How do you acquire a dog?
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex === 2}>
+                        <p>
+                            Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.
+                        </p>
+                        <p>
+                            A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to
+                            {' '}assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your
+                            {' '}dog from a shelter, helps give a good home to a dog who may not find one so readily.
+                        </p>
+                    </Accordion.Content>
+                </Accordion>
+                <Modal basic size='small' trigger={<Button>Basic Modal</Button>}>
+                    <Header icon='archive' content='Archive Old Messages' />
+                    <Modal.Content>
+                        <p>Your inbox is getting full, would you like us to enable automatic archiving of old messages?</p>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button basic color='red' inverted>
+                            <Icon name='remove' /> No
+                        </Button>
+                        <Button color='green' inverted>
+                            <Icon name='checkmark' /> Yes
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
+            </div>
         );
     }
 }
