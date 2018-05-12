c55503cc157d8afaa1a1e31b2faa266777eab31c
Benjamin Müller <benimueller@outlook.com>
2018-05-12 16:45:32 +0200

    Use .net standart

--- Build/generate-webapi-client-standalone.fsx
+++ Build/generate-webapi-client-standalone.fsx
@@ -9,6 +9,7 @@
 #r "./packages/FAKE/tools/NSwag.SwaggerGeneration.WebApi.dll"
 #r "./packages/FAKE/tools/FakeLib.dll"
 #r "./packages/FAKE/tools/DotLiquid.dll"
+#r "./../Backend/src/SSAH.Infrastructure.Api/bin/Release/netstandard2.0/SSAH.Infrastructure.Api.dll"
 
 open Fake
 open System.IO
@@ -16,11 +17,13 @@ open System.IO
 open NSwag.AssemblyLoader
 open NSwag.CodeGeneration.TypeScript
 open NSwag.SwaggerGeneration.WebApi
-open System
+open System.Runtime
+open System.Security.Policy
+open System.Reflection
 
 let projectRoot = __SOURCE_DIRECTORY__ @@ ".."
 
-let generate inputAssembly outputApiTs =
+let generate (inputAssembly: System.Reflection.Assembly) outputApiTs =
   printfn "generate api ts %A to %s" inputAssembly outputApiTs
 
   let settings =
@@ -42,37 +45,48 @@ let generate inputAssembly outputApiTs =
       GenerateOptionalParameters = true
     )
 
-  let resolver = ResolveEventHandler(fun _ args ->
-    let name = args.Name.Substring(0, args.Name.IndexOf(','))
+  //let resolver = ResolveEventHandler(fun _ args ->
+  //  let name = args.Name.Substring(0, args.Name.IndexOf(','))
 
-    if name = "System.Runtime" then
-      let fileName2 = """C:\Program Files\dotnet\shared\Microsoft.NETCore.App\2.0.6\""" + name + ".dll"
+  //  let fileName1 = """""" + name + ".dll"
+  //  // let fileName2 = """C:\Users\ben\.nuget\packages\netstandard.library\2.0.3\build\netstandard2.0\ref""" + name + ".dll"
       
-      if (File.Exists fileName2) then
-        printfn "Resolve %s" fileName2
-        System.Reflection.Assembly.LoadFile(fileName2)
-      else
-        printfn "Resolve %s" "null"
-        null
-    else
-      null
-  )
+  //  if (File.Exists fileName1) then
+  //      printfn "Resolve %s" fileName1
+  //      System.Reflection.Assembly.ReflectionOnlyLoadFrom(fileName1)
+  //  //else if (File.Exists fileName2) then
+  //  //    printfn "Resolve %s" fileName2
+  //  //    System.Reflection.Assembly.ReflectionOnlyLoadFrom(fileName2)
+  //  else
+  //      printfn "Resolve %s" "null"
+  //      null
+  //)
 
-  AppDomain.CurrentDomain.add_ReflectionOnlyAssemblyResolve(resolver)
- 
+  //let bindings = [
+  //  new BindingRedirect("System.Runtime", "4.0.0.0", "b03f5f7f11d50a3a")
+  //]
+  //let preload = [
+  //]
+  //let y = new AppDomainIsolation<AssemblyLoader>("""C:\Code\ssah\Backend\src\SSAH.Infrastructure.Api\bin\Release\netcoreapp2.0\publish\""", null, bindings, preload)
+
+  //let a = y.Domain.GetAssemblies()
+
+  //for asx in a do
+  //  printfn "Lol %s" asx.FullName
+
   tssettings.TypeScriptGeneratorSettings.ExtensionCode <- ReadFileAsString (projectRoot @@ "Build" @@ "generate-webapi-client.extension-code.ts")
   tssettings.TypeScriptGeneratorSettings.TypeScriptVersion <- 2.3m
   tssettings.TypeScriptGeneratorSettings.GenerateCloneMethod <- true
   tssettings.TypeScriptGeneratorSettings.GenerateConstructorInterface <- true
 
-  let controllers = Seq.collect WebApiToSwaggerGenerator.GetControllerClasses [inputAssembly]
+  //let controllers = Seq.filter (fun (x: System.Type) -> x.IsAbstract = false && x.Name.EndsWith("Controller")) []
   let generator = WebApiToSwaggerGenerator(settings)
-  let document = generator.GenerateForControllersAsync(controllers).Result;
+  let document = generator.GenerateForControllersAsync([typeof<SSAH.Infrastructure.Api.Controllers.RegistrationController>]).Result;
   let generator = SwaggerToTypeScriptClientGenerator(document, tssettings)
   let code = generator.GenerateFile()
   File.WriteAllText(outputApiTs, code)
 
 !! "./Backend/src/SSAH.Infrastructure.Api/bin/Release/netcoreapp2.0/publish/SSAH.Infrastructure.Api.dll"
 |> SetBaseDir projectRoot
-|> Seq.map System.Reflection.Assembly.ReflectionOnlyLoadFrom
+|> Seq.map System.Reflection.Assembly.LoadFrom
 |> Seq.iter (fun assembly -> generate assembly (projectRoot @@ "/Frontend/src/app/api.ts"))
