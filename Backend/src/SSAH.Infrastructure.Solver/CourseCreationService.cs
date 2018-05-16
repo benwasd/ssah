88e611ef6c6457432876e4c2cfc9846a5f74bfdf
Benjamin Müller <benimueller@outlook.com>
2018-05-16 14:25:30 +0200

    Enhance permutation generation

--- Backend/src/SSAH.Infrastructure.Solver/CourseCreationService.cs
+++ Backend/src/SSAH.Infrastructure.Solver/CourseCreationService.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
-
-using SSAH.Core.Domain.CourseCreation;
 
 namespace SSAH.Infrastructure.Solver
 {
@@ -11,9 +11,9 @@ namespace SSAH.Infrastructure.Solver
         public static void Main()
         {
             // readonly
-            int[] jahrgangOfTeilnehmer = { 1900, 1989, 2002, 2000 };
-            int[] spracheOfTeilnehmer = { 1, 1, 2, 2 };
-            int[] besuchteKurstageAufGleichemNiveauOfTeilnehmer = { 3, 4, 1, 1 };
+            int[] jahrgangOfTeilnehmer = { 1960, 1990, 1980, 1982, 2001, 2003, 2010, 2004, 2002, 1998, 1999, 1993, 1992 /*, 1991, 1989, 1993, 1950*/ };
+            int[] spracheOfTeilnehmer = { 1, 1, 2, 2, 2, 3, 3, 2, 1, 2/*, 1, 1, 4, 4, 3, 2, 2 */};
+            int[] besuchteKurstageAufGleichemNiveauOfTeilnehmer = { 4, 4, 4, 4, 4, 4, 4, 5, 5, 5/*, 5, 5, 1, 1, 1, 1, 5*/ };
 
             var teilnehmers = Enumerable.Range(0, jahrgangOfTeilnehmer.Length).ToArray(); // 0-3
             var kurse = Enumerable.Range(1, 3).ToArray(); // 1-3
@@ -21,32 +21,92 @@ namespace SSAH.Infrastructure.Solver
             // mutated
             int[] kursOfTeilnehmer; // { 1, 1, 1, 1 };
 
+            Debug.WriteLine("www1 {0}", (object)PatternStamp(new[] { 1, 1 }));
+            Debug.WriteLine("www1 {0}", (object)PatternStamp(new[] { 3, 4 }));
+            Debug.WriteLine("www1 {0}", (object)PatternStamp(new[] { 0, 1, 0, 4 }));
+            Debug.WriteLine("www1 {0}", (object)PatternStamp(new[] { 0, 1, 0, 2 }));
+            Debug.WriteLine("www1 {0}", (object)PatternStamp(new[] { 2, 1, 2, 1, 2 }));
+            Debug.WriteLine("www2 {0}", (object)PatternStamp(new[] { 1, 2, 1, 2, 4 }));
+            Debug.WriteLine("www3 {0}", (object)PatternStamp(new[] { 1, 2, 1, 2, 4 }));
+            Debug.WriteLine("www4 {0}", (object)PatternStamp(new[] { 2, 1, 2, 1, 8 }));
+
             // solve
+            
+
             var solutions = new List<Tuple<int[], double>>();
-            foreach (var x in GetPermutations(kurse, teilnehmers.Length))
+
+            /*
+
+                    13:
+                    Permutationen Start 16.05.2018 11:39:46
+                    Permutationen 265721
+                    Permutationen End 16.05.2018 11:52:04
+                    ~13 min
+
+                    13:
+                    Kombs Start 16.05.2018 11:52:04
+                    Kombs 265721
+                    Kombs End 16.05.2018 11:58:21
+                    ~6 min
+
+                    13 Permutation Util:
+                    Permutationen Start 16.05.2018 13:03:08
+                    Permutationen 265722
+                    Permutationen End 16.05.2018 13:08:09
+                    ~5 min
+
+                    Permutationen Start 16.05.2018 14:24:09
+                    Permutationen 265721
+                    Permutationen End 16.05.2018 14:24:15
+                    ~6 sec
+
+             */
+
+            //Debug.WriteLine("Permutationen Start {0}", DateTime.Now);
+            //var pers = GetPermutationsYield(kurse, teilnehmers.Length).Distinct(new X()).Count();
+            //Debug.WriteLine("Permutationen {0}", pers);
+            //Debug.WriteLine("Permutationen End {0}", DateTime.Now);
+
+            Debug.WriteLine("Permutationen Start {0}", DateTime.Now);
+            var pers2 = GetPermutationsYield2(kurse, teilnehmers.Length).Count();
+            Debug.WriteLine("Permutationen {0}", pers2);
+            Debug.WriteLine("Permutationen End {0}", DateTime.Now);
+
+            //Debug.WriteLine("Kombs Start {0}", DateTime.Now);
+            //var combs = GetKCombsWithRept(kurse, teilnehmers.Length).Distinct(new X()).Count();
+            //Debug.WriteLine("Kombs {0}", combs);
+            //Debug.WriteLine("Kombs End {0}", DateTime.Now);
+
+            /*
+            foreach (var x in GetPermutationsYield2(kurse, teilnehmers.Length))
             {
                 kursOfTeilnehmer = x.ToArray();
 
-                var value = kurse
-                    .Select(k => {
-                        var jahrgang = StandartabweichungJahrgaenge(kursOfTeilnehmer, jahrgangOfTeilnehmer, k);
-                        var sprache = EindeutigeSprachen(kursOfTeilnehmer, spracheOfTeilnehmer, k);
-                        var besuchteKurstageAufGleichemNiveau = StandartabweichungBesuchteKurstageAufGleichemNiveau(kursOfTeilnehmer, besuchteKurstageAufGleichemNiveauOfTeilnehmer, k);
+                Debug.WriteLine(String.Join(",", kursOfTeilnehmer));
 
-                        var valueForCourse = jahrgang * sprache * besuchteKurstageAufGleichemNiveau;
+                //    //var value = kurse
+                //    //    .Select(k =>
+                //    //    {
+                //    //        var jahrgang = StandartabweichungJahrgaenge(kursOfTeilnehmer, jahrgangOfTeilnehmer, k);
+                //    //        var sprache = EindeutigeSprachen(kursOfTeilnehmer, spracheOfTeilnehmer, k);
+                //    //        var besuchteKurstageAufGleichemNiveau = StandartabweichungBesuchteKurstageAufGleichemNiveau(kursOfTeilnehmer, besuchteKurstageAufGleichemNiveauOfTeilnehmer, k);
 
-                        return valueForCourse;
-                    })
-                    .Sum();
+                //    //        var valueForCourse = jahrgang * sprache * besuchteKurstageAufGleichemNiveau;
 
-                solutions.Add(Tuple.Create(kursOfTeilnehmer, value));
+                //    //        return valueForCourse;
+                //    //    })
+                //    //    .Sum();
+
+                //    //solutions.Add(Tuple.Create(kursOfTeilnehmer, value));
             }
+            */
 
             var bestSolutions = solutions.OrderBy(s => s.Item2).Take(10).ToArray();
 
             Console.ReadLine();
         }
 
+        /*
         public static IEnumerable<IEnumerable<T>> GetPermutations<T>(ICollection<T> list, int length)
         {
             if (length == 1)
@@ -55,7 +115,165 @@ namespace SSAH.Infrastructure.Solver
             }
 
             return GetPermutations(list, length - 1)
-                .SelectMany(t => list.Where(e => true), (t1, t2) => t1.Concat(new T[] { t2 }));
+                .SelectMany(t => list.Select(t2 => t.Concat(new T[] { t2 })));
+        }
+
+        public static IEnumerable<ICollection<int>> GetPermutationsYield(ICollection<int> list, int count)
+        {
+            if (count == 0)
+            {
+                yield return new int[0];
+            }
+            else
+            {
+                foreach (var element in list)
+                {
+                    foreach (var subElements in GetPermutationsYield(list, count - 1))
+                    {
+                        yield return Concat(element, subElements).ToArray();
+                    }
+                }
+            }
+        }
+        */
+
+        public static IEnumerable<ICollection<int>> GetPermutationsYield2(ICollection<int> list, int count)
+        {
+            if (count == 0)
+            {
+                yield return Array.Empty<int>();
+            }
+            else
+            {
+                var producedPatterns = new HashSet<string>();
+
+                foreach (var element in list)
+                {
+                    foreach (var subElements in GetPermutationsYield2(list, count - 1))
+                    {
+                        var x = Concat(element, subElements).ToArray();
+                        var pattern = PatternStamp(x);
+
+                        if (producedPatterns.Add(pattern))
+                        {
+                            yield return x;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<T> Concat<T>(T a, IEnumerable<T> seqb)
+        {
+            yield return a;
+
+            foreach (T b in seqb)
+            {
+                yield return b;
+            }
+        }
+
+        /*
+
+        public class X : EqualityComparer<Col>
+        {
+            public override bool Equals(Col x, Col y)
+            {
+                return x.Changes == y.Changes;
+            }
+
+            public override int GetHashCode(Col obj)
+            {
+                return obj.Changes.GetHashCode();
+            }
+
+            // 2 2, 3 3 == true
+            // 1 2, 3 2 == true
+            // 1 2, 1 1 == false
+            // 1 2 3, 3 2 1 == true
+            // 1 2 1, 3 2 1 == false
+            public static bool IsSameCombination(IEnumerable<int> seqa, IEnumerable<int> seqb)
+            {
+                int? alast = null;
+                int? blast = null;
+
+                Dictionary<int, int> equalities = new Dictionary<int, int>();
+
+                foreach (var zip in seqa.Zip(seqb, (a, b) => new { a, b }))
+                {
+                    var achange = alast != zip.a;
+                    var bchange = blast != zip.b;
+
+                    if (achange != bchange)
+                    {
+                        return false;
+                    }
+
+                    equalities[alast.GetValueOrDefault(-1)] = blast.GetValueOrDefault(-1);
+
+                    var aisknown = equalities.ContainsKey(zip.a);
+                    var bisknown = equalities.ContainsValue(zip.b);
+
+                    if (aisknown != bisknown)
+                    {
+                        return false;
+                    }
+
+                    if (aisknown)
+                    {
+                        if (equalities[zip.a] != zip.b)
+                        {
+                            return false;
+                        }
+                    }
+
+                    alast = zip.a;
+                    blast = zip.b;
+                }
+
+                return true;
+            }
+
+        }
+
+        */
+
+        // 1 2 3 = "012"
+        // 1 2 1 = "010"
+        // 3 2 1 = "012"
+        // 1 1 2 = "001"
+        // 1 2 1 2 3 = "01012"
+        public static string PatternStamp(int[] seq)
+        {
+            HashSet<int> known = new HashSet<int>(capacity: seq.Length);
+            int cnt = 0;
+
+            string res = "";
+
+            foreach (var a in seq)
+            {
+                if (known.Add(a))
+                {
+                    res += cnt;
+                    cnt++;
+                }
+                else
+                {
+                    int index = 0;
+
+                    foreach (var kw in known)
+                    {
+                        if (kw == a)
+                        {
+                            res += index;
+                        }
+
+                        index++;
+                    }
+                }
+            }
+
+            return res;
         }
 
         public static double StandartabweichungJahrgaenge(int[] kursOfTeilnehmer, int[] jahrgangOfTeilnehmer, int kursFilter)
@@ -93,16 +311,6 @@ namespace SSAH.Infrastructure.Solver
                 .ToList();
 
             return Standardabweichung(besuchteKurstageAufGleichemNiveau, 3) + 1;
-        }
-
-        public static void ForEach(this int[] array, Action<int, int> each)
-        {
-            int index = 0;
-            foreach (var item in array)
-            {
-                each(index, item);
-                index++;
-            }
         }
 
         public static double Standardabweichung(List<int> Liste, int Kommastellen)
