f4e9c1dc3a8bacd1521da4f93906807feaa5ca98
Benjamin Müller <benimueller@outlook.com>
2018-05-21 11:18:23 +0200

    Add instructor controller

---
+++ Backend/src/SSAH.Infrastructure.Api/Controllers/InstructorController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SSAH.Infrastructure.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class InstructorController : Controller
+    {
+    }
+}
