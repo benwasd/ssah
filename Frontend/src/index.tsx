c1efee1c5ee5931e67c86322b97614e19f417fe8
Benjamin Müller <benimueller@outlook.com>
2018-05-23 18:27:14 +0200

    Add bootstraps layout helpers

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom';
 
 import './styling/semantic.less';
 import './styling/ssah.less';
+import './styling/bootstrap-utils.less';
 import 'moment/locale/de-ch';
 
 import { App } from './main/containers/App';
