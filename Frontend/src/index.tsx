63b7a73e3cc41d37f3d3c6b7a9dfde7b2159fd07
Benjamin Müller <benimueller@outlook.com>
2018-05-23 14:28:41 +0200

    Remove unused override files

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import './styling/semantic.less';
+import './styling/ssah.less';
 import 'moment/locale/de-ch';
 
 import { App } from './main/containers/App';
