d79af8517654dd1e215cdd4da790794929a25598
Benjamin Müller <benimueller@outlook.com>
2018-05-23 11:27:03 +0200

    Refactor bootstrapper

--- Backend/src/SSAH.Infrastructure.Api/WebApiBootstrapper.cs
+++ Backend/src/SSAH.Infrastructure.Api/WebApiBootstrapper.cs
@@ -1,7 +1,9 @@
 ﻿using Autofac;
 
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,20 +22,11 @@ namespace SSAH.Infrastructure.Api
         public static void AddSnowSchoolAdministrationHub(this IServiceCollection services)
         {
             services.AddCors();
-            services.AddMvc()
-                .AddJsonOptions(options =>
-                {
-                    options.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
-                    options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
-                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-                });
-
-            // Add custom controller activator
-            services.Replace(ServiceDescriptor.Transient<IControllerActivator, UnitOfWorkControllerActivator>());
-
+            services.AddMvc().AddJsonOptions(ConfigureJsonSerializer);
             services.AddSignalR();
 
-            // Add custom hub activator
+            // Add custom controller and hub activator
+            services.Replace(ServiceDescriptor.Transient<IControllerActivator, UnitOfWorkControllerActivator>());
             services.Replace(ServiceDescriptor.Transient<IHubActivator<CourseChangeHub>>(p => new AutofacContainerHubActivator<CourseChangeHub>(p)));
         }
 
@@ -41,14 +34,7 @@ namespace SSAH.Infrastructure.Api
         {
             app.UseScopeMiddleware(container.Resolve<IUnitOfWorkFactory<ILifetimeScope>>());
             app.UseGlobalExceptionHandler();
-
-            app.UseCors(corsPolicyBuilder =>
-            {
-                corsPolicyBuilder.AllowAnyOrigin()
-                    .AllowAnyHeader()
-                    .AllowAnyMethod()
-                    .AllowCredentials();
-            });
+            app.UseCors(ConfigureCorsUsage);
 
             if (env.IsDevelopment())
             {
@@ -56,7 +42,27 @@ namespace SSAH.Infrastructure.Api
             }
 
             app.UseMvc();
-            app.UseSignalR(hr => hr.MapHub<CourseChangeHub>("/courseChange"));
+            app.UseSignalR(ConfigureSignalRUsage);
+        }
+
+        private static void ConfigureJsonSerializer(MvcJsonOptions options)
+        {
+            options.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+            options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
+            options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+        }
+
+        private static void ConfigureCorsUsage(CorsPolicyBuilder builder)
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        }
+
+        public static void ConfigureSignalRUsage(HubRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapHub<CourseChangeHub>("/courseChange");
         }
     }
 }
