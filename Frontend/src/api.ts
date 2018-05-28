8104933d12af64171dd47328b30d5b7c4c622f26
Benjamin Müller <benimueller@outlook.com>
2018-05-28 21:55:16 +0200

    Update assets

--- Frontend/src/api.ts
+++ Frontend/src/api.ts
@@ -169,39 +169,6 @@ export class RegistrationApiProxy extends ApiProxyBase {
         this.baseUrl = baseUrl ? baseUrl : this.getBaseUrl("");
     }
 
-    send(): Promise<void> {
-        let url_ = this.baseUrl + "/api/Registration/Send";
-        url_ = url_.replace(/[?&]$/, "");
-
-        let options_ = <RequestInit>{
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json", 
-            }
-        };
-
-        return this.transformOptions(options_).then(transformedOptions_ => {
-            return this.http.fetch(url_, transformedOptions_);
-        }).then((_response: Response) => {
-            return this.transformResult(url_, _response, (_response: Response) => this.processSend(_response));
-        });
-    }
-
-    protected processSend(response: Response): Promise<void> {
-        const status = response.status;
-        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
-        if (status === 200) {
-            return response.text().then((_responseText) => {
-            return;
-            });
-        } else if (status !== 200 && status !== 204) {
-            return response.text().then((_responseText) => {
-            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
-            });
-        }
-        return Promise.resolve<void>(<any>null);
-    }
-
     getRegistration(registrationId: string): Promise<RegistrationDto> {
         let url_ = this.baseUrl + "/api/Registration/GetRegistration?";
         if (registrationId === undefined || registrationId === null)
